91972a8ef6851eb924119ca9f4fe828925d1e449
Nathan Darcy <nathandarcy33@gmail.com>
2025-12-30 10:39:16 -0600

    add react-icons. add top nav bar with route links - Nathan

---
+++ app/components/Navbar.tsx
@@ -0,0 +1,28 @@
+import { FaLaptopCode } from 'react-icons/fa'
+import { NavLink } from 'react-router'
+
+export default function NavBar() {
+  return (
+    <nav className="bg-gray-800 border-b border-gray-700 shadow-md sticky top-0 z-50">
+      <div className="max-w-6xl mx-auto px-6 py-4 flex justify-between items-center">
+        <NavLink
+          to="/"
+          className="flex items-center gap-2 text-lg font-bold text-blue-300"
+        >
+          <FaLaptopCode className="text-blue-400 text-xl" />
+          <span>The Friendly Dev</span>
+        </NavLink>
+
+        <div className="hidden md:flex items-center gap-6">
+          <div className="space-x-4 text-sm text-gray-300">
+            <NavLink to="/">Home</NavLink>
+            <NavLink to="/project">Projects</NavLink>
+            <NavLink to="/blog">Blog</NavLink>
+            <NavLink to="/about">About</NavLink>
+            <NavLink to="/contact">Contact</NavLink>
+          </div>
+        </div>
+      </div>
+    </nav>
+  )
+}
