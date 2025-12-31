95ee994ff44c87371838d276ff814f2a2d9586e8
Nathan Darcy <nathandarcy33@gmail.com>
2025-12-31 09:11:36 -0600

    display data on the screen - Nathan

--- app/routes/projects/index.tsx
+++ app/routes/projects/index.tsx
@@ -1,5 +1,6 @@
 import type { Project } from '~/types'
 import type { Route } from './+types'
+import ProjectCard from '~/components/ProjectCard'
 
 export async function loader({
   request,
@@ -17,10 +18,16 @@ export async function loader({
 
 export default function ProjectPage({ loaderData }: Route.ComponentProps) {
   const { projects } = loaderData
-  console.log(projects)
+
   return (
     <>
       <h2 className="text-3xl text-white font-bold mb-8">Projects</h2>
+
+      <div className="grid gap-6 sm:grid-cols-2">
+        {projects.map((project) => (
+          <ProjectCard key={project.id} project={project} />
+        ))}
+      </div>
     </>
   )
 }
