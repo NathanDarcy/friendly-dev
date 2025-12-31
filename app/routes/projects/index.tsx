1f93472de565d9162fb3bb324e3fb718a148614d
Nathan Darcy <nathandarcy33@gmail.com>
2025-12-31 08:53:01 -0600

    set up fake data call - Nathan

--- app/routes/projects/index.tsx
+++ app/routes/projects/index.tsx
@@ -1,4 +1,23 @@
-export default function ProjectPage() {
+import type { Project } from '~/types'
+import type { Route } from './+types'
+
+export async function loader({
+  request,
+}: Route.LoaderArgs): Promise<{ projects: Project[] }> {
+  if (request) {
+    console.log(request)
+  }
+  const response = await fetch('http:localhost:8000/projects')
+  const data = await response.json()
+
+  return {
+    projects: data,
+  }
+}
+
+export default function ProjectPage({ loaderData }: Route.ComponentProps) {
+  const { projects } = loaderData
+  console.log(projects)
   return (
     <>
       <h2 className="text-3xl text-white font-bold mb-8">Projects</h2>
