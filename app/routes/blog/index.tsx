5313430fbe7c0c8264ffb04fd6187d71fe304e4c
Nathan Darcy <nathandarcy33@gmail.com>
2026-01-07 10:58:45 -0600

    implement post filter - Nathan

--- app/routes/blog/index.tsx
+++ app/routes/blog/index.tsx
@@ -3,6 +3,7 @@ import type { Route } from './+types/index'
 import PostCard from '~/components/PostCard'
 import { useState } from 'react'
 import Pagination from '~/components/Pagination'
+import PostFilter from '~/components/PostFilter'
 
 export async function loader({
   request,
@@ -27,23 +28,45 @@ export async function loader({
 }
 
 export default function BlogPage({ loaderData }: Route.ComponentProps) {
+  const [searchQuery, setSearchQuery] = useState('')
   const [currentPage, setCurrentPage] = useState(1)
   const POSTS_PER_PAGE = 5
 
   const { posts } = loaderData
 
-  const totalPages = Math.ceil(posts.length / POSTS_PER_PAGE)
+  const filteredPosts = posts.filter((post) => {
+    const query = searchQuery.toLowerCase()
+
+    return (
+      post.title.toLowerCase().includes(query) ||
+      post.excerpt.toLowerCase().includes(query)
+    )
+  })
+
+  const totalPages = Math.ceil(filteredPosts.length / POSTS_PER_PAGE)
   const lastPageIndex = currentPage * POSTS_PER_PAGE
   const firstPageIndex = lastPageIndex - POSTS_PER_PAGE
-  const currentPosts = posts.slice(firstPageIndex, lastPageIndex)
+  const currentPosts = filteredPosts.slice(firstPageIndex, lastPageIndex)
 
   return (
     <div className="max-w-3xl mx-auto mt-10 px-6 py-6 bg-gray-900">
       <h2 className="text-3xl text-white font-bold mb-8">Blog</h2>
 
-      {currentPosts.map((post) => (
-        <PostCard key={post.slug} post={post} />
-      ))}
+      <PostFilter
+        searchQuery={searchQuery}
+        onSearchChange={(query) => {
+          setSearchQuery(query)
+          setCurrentPage(1)
+        }}
+      />
+
+      <div className="space-y-8">
+        {currentPosts.length === 0 ? (
+          <p className="text-gray-400 text-center"> No Posts Found</p>
+        ) : (
+          currentPosts.map((post) => <PostCard key={post.slug} post={post} />)
+        )}
+      </div>
 
       {totalPages > 1 && (
         <Pagination
