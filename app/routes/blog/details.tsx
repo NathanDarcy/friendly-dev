966ae1dcfa4f5c7c0a0b98483e358e5c69deda72
Nathan Darcy <nathandarcy33@gmail.com>
2026-01-07 10:36:14 -0600

    Blog page impl with a new post

--- app/routes/blog/details.tsx
+++ app/routes/blog/details.tsx
@@ -1,5 +1,7 @@
 import type { PostMeta } from '~/types'
 import type { Route } from './+types/details'
+import ReactMarkdown from 'react-markdown'
+import { Link } from 'react-router'
 
 export type BlogPostDetailsPageProps = {
   loaderData: {
@@ -38,7 +40,25 @@ export default function BlogPostDetailsPage({
   loaderData,
 }: BlogPostDetailsPageProps) {
   const { postMeta, markdown } = loaderData
-  console.log(postMeta, markdown)
 
-  return <>Blog</>
+  return (
+    <div className="max-w-3xl mx-auto px-6 py-12 bg-gray-900">
+      <h1 className="text-3xl font-bold text-blue-400 mb2">{postMeta.title}</h1>
+
+      <p className="text-sm text-gray-400 mb-6">
+        {new Date(postMeta.date).toLocaleDateString()}
+      </p>
+
+      <div className="prose prose-invert max-w-none mb-12">
+        <ReactMarkdown>{markdown}</ReactMarkdown>
+      </div>
+
+      <Link
+        to="/blog"
+        className="inline-block bg-blue-600 text-white px-6 py-2 rounded-lg hover:bg-blue-700 transition"
+      >
+        {'<- '}Back to Posts
+      </Link>
+    </div>
+  )
 }
